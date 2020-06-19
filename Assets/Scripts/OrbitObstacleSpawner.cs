850cb5b0b312eb0aad4ccd56d2d7354c478554c8
Dmitry <divgorbunov@gmail.com>
2020-06-20 02:05:39 +0300

    Add ObstacleSpawner

---
+++ Assets/Scripts/OrbitObstacleSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitObstacleSpawner : MonoBehaviour
+{
+    public OrbitController orbit;
+    public GameObject ObstaclePrefab;
+
+    public int maxCount = 3;
+
+    public static List<GameObject> activeObstacles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        activeObstacles = new List<GameObject>();
+        for (int i = 0; i < maxCount; i++)
+        {
+            Vector3 position = orbit.GetRandomPointOnPerimeter();
+           
+            GameObject obstacle = Instantiate(ObstaclePrefab, position, Quaternion.identity);
+            activeObstacles.Add(obstacle);
+        }
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/SpacecraftController.cs
+++ Assets/Scripts/SpacecraftController.cs
@@ -24,5 +24,45 @@ public class SpacecraftController : MonoBehaviour
         var Y = y + (b * Mathf.Sin(alpha * .005f));
         gameObject.transform.position = new Vector3(X, 0, Y);
         gameObject.transform.right = gameObject.transform.position - new Vector3(x, 0, y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Pressed shot");
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        //right now just search for closest obstacle
+
+        float closest = 100.0f;
+        GameObject closestObject = null;
+        foreach(var o in OrbitObstacleSpawner.activeObstacles)
+        {
+            float objDistance = Vector3.Distance(this.gameObject.transform.position, o.transform.position);
+
+            if (objDistance < closest)
+            {
+                closest = objDistance;
+                closestObject = o;
+            }
+        }
+        Debug.Log("Closest is: " + closest);
+        if (closest<3.0f)
+        {
+            OrbitObstacleSpawner.activeObstacles.Remove(closestObject);
+            Destroy(closestObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("Trigger");
+        if (collider.gameObject.tag == "Obstacle")
+        {
+            Debug.Log("Obstacle");
+            Destroy(gameObject);
+        }
     }
 }
