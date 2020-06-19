0e6acda21fbb6a57fb84a4769937176a4bb63254
Sanyo <sanyo.gorbunov@gmail.com>
2020-06-19 23:07:30 +0200

    Obstacle and random object

--- Assets/Scripts/OrbitController.cs
+++ Assets/Scripts/OrbitController.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class OrbitController : MonoBehaviour
 {
@@ -12,12 +10,19 @@ public class OrbitController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public Vector3 GetRandomPointOnPerimeter()
+    {
+        var alpha = Random.Range(0, 360);
+        var x = X + (A * Mathf.Cos(alpha));
+        var y = Y + (B * Mathf.Sin(alpha));
+        return new Vector3(x, 0, y);
     }
 }
