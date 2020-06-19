452e26a9abf96a683384e38d2a2267128e75efaa
Sanyo <sanyo.gorbunov@gmail.com>
2020-06-19 22:56:47 +0200

    Added orbit

---
+++ Assets/Scripts/OrbitController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitController : MonoBehaviour
+{
+    public float X => gameObject.transform.position.x;
+    public float Y => gameObject.transform.position.z;
+    public float A => gameObject.transform.localScale.x / 2;
+    public float B => gameObject.transform.localScale.z / 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/SpacecraftController.cs
+++ Assets/Scripts/SpacecraftController.cs
@@ -1,18 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SpacecraftController : MonoBehaviour
 {
+    private float x, y, a, b;
+    public OrbitController currentOrbit;
+
+    private float alpha;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        x = currentOrbit.X;
+        y = currentOrbit.Y;
+        a = currentOrbit.A;
+        b = currentOrbit.B;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        alpha += 1;
+        var X = x + (a * Mathf.Cos(alpha * .005f));
+        var Y = y + (b * Mathf.Sin(alpha * .005f));
+        gameObject.transform.position = new Vector3(X, 0, Y);
+        gameObject.transform.right = gameObject.transform.position - new Vector3(x, 0, y);
     }
 }
